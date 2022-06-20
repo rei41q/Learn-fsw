a9a653913e7271afc7125e18de2baef9f28264cc
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-06-20 20:58:01 +0700

    core module nodejs

---
+++ cp5/01_nodejs/sayHai.js
@@ -0,0 +1,16 @@
+// console.log("Hai");
+
+// =================== CORE MODULE ================
+// const fs = require("fs");
+
+// const readingFile = fs.readFileSync("./README.txt", "utf-8");
+
+// console.log(readingFile);
+
+const os = require("os");
+
+const readingLocation = os.homedir();
+
+console.log(readingLocation);
+
+// =================== END CORE MODULE ================
