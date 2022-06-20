76dd96843750ed30209ddef514157ece987da8c7
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-06-20 21:38:42 +0700

    local module nodejs

--- cp5/01_nodejs/sayHai.js
+++ cp5/01_nodejs/sayHai.js
@@ -7,10 +7,31 @@
 
 // console.log(readingFile);
 
-const os = require("os");
+// const operatingSystem = require("os");
 
-const readingLocation = os.homedir();
+// const readingLocation = operatingSystem.homedir();
 
-console.log(readingLocation);
+// console.log(readingLocation);
 
 // =================== END CORE MODULE ================
+
+// =================== LOCAL MODULE ================
+
+// const calculator = require("./utils/calculator");
+// const { addition, substract } = require("./utils/calculator");
+
+// const tambahkan = calculator.addition(2, 3);
+// const kurangkan = calculator.substract(3, 2);
+
+// const tambahkan = addition(2, 3);
+// const kurangkan = substract(3, 2);
+
+// console.log(`tambahkan 2 dan 3 = ${tambahkan}`);
+// console.log(`kurangkan 3 dan 2 = ${kurangkan}`);
+// console.log(`kalikan 3 dan 2 = ${kalikan}`);
+
+// =================== END LOCAL MODULE ================
+
+
+// =================== 3rd PARTY MODULE ================
+
