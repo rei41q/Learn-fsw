9e04c5c1b7fab11617f65d4bdca0951dc562b670
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-07-18 20:50:15 +0700

    add migration

---
+++ cp6/belajar_implementasi/src/database/seeders/20220718131928-create-super-user.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("Users", [
+      {
+        username: "superuser",
+        password: "iamsuperuser",
+        is_active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
