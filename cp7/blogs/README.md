610aa8ff32b930ac3a08c6e0377b6efdcc20cbb2
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-08-03 14:03:35 +0700

    basic blog app

---
+++ cp7/blogs/README.md
@@ -0,0 +1,8 @@
+The purpose of this repository is to learn basic setup and learn how to build a blog application using express. 
+
+How To Run : 
+1. Copy the repository
+2. Go inside the repository
+3. npm install
+4. npx sequelize-cli db:migrate
+5. npm run dev
---
+++ cp7/blogs/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const userRouter = require("./src/user/user.route");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello from simple server :)");
+});
+
+app.use(userRouter);
+
+app.listen(port, () =>
+  console.log("> Server is up and running on port : " + port)
+);
