f2fbb77186c5c69a307b38e4df825b831ab17d7e
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-08-04 20:36:40 +0700

    implementasi jwt token verification

--- cp7/blogs/app.js
+++ cp7/blogs/app.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const authRouter = require("./src/auth/auth.route");
 const userRouter = require("./src/user/user.route");
 const app = express();
-require("dotenv").config();
+
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
--- cp7/blogs/src/user/user.route.js
+++ cp7/blogs/src/user/user.route.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const tokenVerification = require("../middleware/token.verification");
 const userRouter = express.Router();
 
 const userController = require("./user.controller");
 
 userRouter.post("/user/registration", userController.createUser);
+
+// edit nama user
+userRouter.put("/user/:userId", tokenVerification, (req, res) => {
+  const { userId } = req.params;
+  const authUser = req.auth;
+
+  if (authUser.id != userId) {
+    return res.send("Gak boleh edit, id ini bukan punyamu");
+  }
+
+  return res.send(`Mau edit nama user, id ${userId}`);
+});
 
 module.exports = userRouter;
