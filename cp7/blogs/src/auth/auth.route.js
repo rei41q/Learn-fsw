691249c68c60e09db459e8bf813ca8fff657d94f
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-08-08 21:36:09 +0700

    adding validation request

--- cp7/blogs/src/auth/auth.route.js
+++ cp7/blogs/src/auth/auth.route.js
@@ -4,32 +4,47 @@ const authRouter = express.Router();
 const { User } = require("../database/models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
 
-authRouter.post("/auth/login", async (req, res) => {
-  const { email, password } = req.body;
-  const existUser = await User.findOne({ where: { email }, raw: true });
+authRouter.post(
+  "/auth/login",
+  body("title").isEmail(),
+  body("password").isStrongPassword(),
+  async (req, res) => {
+    // check apakah ada error dalam request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  //   kalau gak ada email yang terdaftar response not found
-  if (!existUser) return res.status(404).json({ message: "User not found" });
+    const { email, password } = req.body;
+    const existUser = await User.findOne({ where: { email }, raw: true });
 
-  //   kalau ada cek password
-  const isPasswordCorrect = await bcrypt.compare(password, existUser.password);
-  if (isPasswordCorrect) {
-    // generate jwt token
-    const token = await jwt.sign(
-      {
-        id: existUser.id,
-        fullname: existUser.fullname,
-        email: existUser.email,
-      },
-      process.env.JWT_SECRET_TOKEN,
-      { expiresIn: "1d" }
+    //   kalau gak ada email yang terdaftar response not found
+    if (!existUser) return res.status(404).json({ message: "User not found" });
+
+    //   kalau ada cek password
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existUser.password
     );
+    if (isPasswordCorrect) {
+      // generate jwt token
+      const token = await jwt.sign(
+        {
+          id: existUser.id,
+          fullname: existUser.fullname,
+          email: existUser.email,
+        },
+        process.env.JWT_SECRET_TOKEN,
+        { expiresIn: "1d" }
+      );
 
-    return res.json({ accessToken: token });
-  } else {
-    return res.send("Login failed");
+      return res.json({ accessToken: token });
+    } else {
+      return res.send("Login failed");
+    }
   }
-});
+);
 
 module.exports = authRouter;
