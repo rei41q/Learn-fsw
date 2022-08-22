709575a9b078e3beff939335b0a58a664c2411d0
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-08-22 20:17:07 +0700

    initiate react

---
+++ cp8/chapter8/src/App.js
@@ -0,0 +1,14 @@
+import Identity from "./Identity";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Halaman React Pertamaku</h1>
+      </div>
+      <Identity />
+    </>
+  );
+}
+
+export default App;
---
+++ cp8/chapter8/src/Identity.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+class Identity extends Component {
+  render() {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td>Nama</td>
+            <td>:</td>
+            <td>El Setiyawan</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Identity;
