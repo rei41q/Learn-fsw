6fc1ec29984563c635a0fa264421e42e3f78e568
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-08-29 20:22:03 +0700

    routing 2

--- cp8/chapter8/src/App.js
+++ cp8/chapter8/src/App.js
@@ -1,25 +1,54 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Blogs from "./pages/Blogs/Blogs";
 import Education from "./pages/Education";
 import Identity from "./pages/Identity";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<div>Ini halaman home</div>} />
-      <Route
-        path="/identity"
-        element={<Identity user={{ name: "El Setiyawan", address: "Jogja" }} />}
-      />
-      <Route
-        path="/education"
-        element={
-          <Education
-          educations={{ univ: "UMY", jurusan: "Hubungan International" }}
-          />
-        }
-      />
-    </Routes>
+    <>
+      <nav style={{ backgroundColor: "lightgreen" }}>
+        <ul style={{ listStyleType: "none" }}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/identity">Identity</Link>
+          </li>
+          <li>
+            <Link to="/education">Education</Link>
+          </li>
+        </ul>
+      </nav>
+      <div
+        style={{
+          backgroundColor: "lightgreen",
+          padding: "10px",
+        }}
+      >
+        <div style={{ backgroundColor: "white", minHeight: "200px" }}>
+          <Routes>
+            <Route path="/" element={<div>Ini halaman home</div>} />
+            <Route
+              path="/identity"
+              element={
+                <Identity user={{ name: "El Setiyawan", address: "Jogja" }} />
+              }
+            />
+            <Route
+              path="/education"
+              element={
+                <Education
+                  educations={{
+                    univ: "UMY",
+                    jurusan: "Hubungan International",
+                  }}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </div>
+    </>
   );
 };
 
