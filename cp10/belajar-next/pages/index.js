092f81737d655ca90c4d845124d360dcb2304d59
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-10-05 19:36:47 +0700

    add bootstrap

--- cp10/belajar-next/pages/index.js
+++ cp10/belajar-next/pages/index.js
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div>Welcome Screen!</div>;
+  return (
+    <div>
+      <div>Welcome Screen!</div>
+      <div>
+        {" "}
+        <button className="btn btn-primary">Sign in</button>
+      </div>
+    </div>
+  );
 }
